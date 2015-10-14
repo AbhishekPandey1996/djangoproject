e20ba0a51dcb5643b494cf94e082441ad35e6cbd
AbhishekPandey1996 <abhishek.ramakant.che14@iitbhu.ac.in>
2015-10-15 04:49:52 +0530

    adding templates

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    context_dict = { 'boldmessage': "I am Bold From Context"}
+    return render(request, 'rango/index.html', context_dict)
+#    return HttpResponse("Hello World from first page!")
